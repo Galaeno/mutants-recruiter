aabb0015adbe612fe72a8fc80a9039930d40b0bb
Gonzalo Henriquez <gonzalo@fktech.net>
2020-11-02 14:47:54 -0300

    Se agregan archivos faltantes

--- build/src/utils/constants/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER = void 0;
-const appConfig_json_1 = require("../../../appConfig.json");
-exports.SERVER = appConfig_json_1.server || 'Restify';
-//# sourceMappingURL=config.js.map
